997599108b6745bf737b8d24600d6a2c2e0d6fcd
Ciraq Resulov <ciraq.resulov@gmail.com>
2019-04-03 16:03:42 +0400

    Adding new file

---
+++ ExchangeRate/Currency.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ExchangeRate
+{
+    public static class Currency
+    {
+        public static string FetchData(string date, DataGridView dataGrid)
+        {
+            string url = "https://www.cbar.az/currencies/" + date + ".xml";
+            XElement root = XElement.Load(url);
+            IEnumerable<XElement> valute =
+                from el in root.Elements("ValType")
+                where (string)el.Attribute("Type") == "Xarici valyutalar"
+                select el;
+
+            var query = from key in valute.Descendants("Valute")
+                        select new
+                        {
+                            Kod = key.Attribute("Code").Value,
+                            Nominal = key.Element("Nominal").Value,
+                            Valyuta = key.Element("Name").Value,
+                            Kurs = key.Element("Value").Value
+                        };
+            dataGrid.DataSource = query.ToList();
+            return string.Empty;
+        }
+    }
+}
