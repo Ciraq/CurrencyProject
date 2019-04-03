bd060ea7e6f16e5b358fc57ce5c4fcceba751ce8
Ciraq Resulov <ciraq.resulov@gmail.com>
2019-04-03 11:07:57 +0400

    commit

---
+++ ExchangeRate/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ExchangeRate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnDateSelected = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 11);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // btnDateSelected
+            // 
+            this.btnDateSelected.Location = new System.Drawing.Point(244, 11);
+            this.btnDateSelected.Name = "btnDateSelected";
+            this.btnDateSelected.Size = new System.Drawing.Size(97, 23);
+            this.btnDateSelected.TabIndex = 1;
+            this.btnDateSelected.Text = "Tarixə görə axtar";
+            this.btnDateSelected.UseVisualStyleBackColor = true;
+            this.btnDateSelected.Click += new System.EventHandler(this.btnDateSelected_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 378);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDateSelected);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnDateSelected;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ExchangeRate/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ExchangeRate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            string mydatetimestr = dateTime.ToString("dd-MM-yyyy").Replace("-", ".");
+
+            string url = "https://www.cbar.az/currencies/" + mydatetimestr + ".xml";
+            XElement root = XElement.Load(url);
+            IEnumerable<XElement> valute =
+                from el in root.Elements("ValType")
+                where (string)el.Attribute("Type") == "Xarici valyutalar"
+                select el;
+
+            var query = from key in valute.Descendants("Valute")
+                        select new
+                        {
+                            Kod = key.Attribute("Code").Value,
+                            Nominal = key.Element("Nominal").Value,
+                            Valyuta = key.Element("Name").Value,
+                            Kurs = key.Element("Value").Value
+                        };
+
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        private void btnDateSelected_Click(object sender, EventArgs e)
+        {
+            string dateselected = dateTimePicker1.Value.Date.ToString("dd/MM/yyyy").Replace("/", ".");
+            string url = "https://www.cbar.az/currencies/" + dateselected + ".xml";
+            XElement root = XElement.Load(url);
+            IEnumerable<XElement> valute =
+                from el in root.Elements("ValType")
+                where (string)el.Attribute("Type") == "Xarici valyutalar"
+                select el;
+
+            var query = from key in valute.Descendants("Valute")
+                        select new
+                        {
+                            Kod = key.Attribute("Code").Value,
+                            Nominal = key.Element("Nominal").Value,
+                            Valyuta = key.Element("Name").Value,
+                            Kurs = key.Element("Value").Value
+                        };
+
+            dataGridView1.DataSource = query.ToList();
+        }
+    }
+}
